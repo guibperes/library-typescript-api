60c73ac4e490fe1ba3bbdd64ed7e87d42cd80f2b
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-05-22 12:50:14 -0300

    logger lib created

---
+++ src/libs/index.ts
@@ -0,0 +1,1 @@
+export { logger, loggerMiddleware } from './logger';
--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,19 @@ import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
+import { logger, loggerMiddleware } from './libs';
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+server.use(loggerMiddleware);
 
 const start = async () => {
   try {
-    server.listen(5000, () => console.log('Server is running on port 5000'));
+    server.listen(5000, () => logger.info('Server is running on port 5000'));
   } catch (error) {
-    console.log(error);
+    logger.info(error);
     process.exit(1);
   }
 };
